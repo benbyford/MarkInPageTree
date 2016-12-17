a5e3ea129ad760d8eb7f1d25a0522b499426fc03
Ben Byford <benjaminbyford@gmail.com>
2016-12-17 15:52:59 +0000

    addition of multiple fields or template to check and then add icon | addition of showing data of a field in page tree

--- MarkInPageTreeConfig.php
+++ MarkInPageTreeConfig.php
@@ -5,8 +5,9 @@ class MarkInPageTreeConfig extends ModuleConfig{
 	// default array for PageDeferredPublish module
 	public function getDefaults() {
 	    return array(
-	      'selector' => '',
-	      'iconName' => 'certificate'
+	      'field' => '',
+		  'template' => '',
+		  'data_field' => ''
 	    );
 	}
 
@@ -24,20 +25,31 @@ class MarkInPageTreeConfig extends ModuleConfig{
 
 		// get InputfieldText module
 		// add fieldname field to settings
-    	$f = $this->modules->get('InputfieldSelector');
-    	$f->attr('name', 'selector');
-    	$f->label = 'Selector';
-		$f->description = 'Selector to select marked pages.';
-
+    	$f = $this->modules->get('InputfieldTextarea');
+    	$f->attr('name', 'field');
+    	$f->label = 'Field';
+		$f->description = "Mark pages when field is true, add pairs of values on each line delimited by a comma,\ne.g. star, exclusive";
 		// add cron option to inputfields
 		$inputfields->add($f);
 
-		// add icon name to settings (to display fontawesome icon to user)
-    	$f = $this->modules->get('InputfieldIcon');
-    	$f->attr('name', 'iconName');
-    	$f->label = 'Icon';
+
+    	$f = $this->modules->get('InputfieldTextarea');
+    	$f->attr('name', 'template');
+    	$f->label = 'Template';
+		$f->description = "Mark pages with specific template, add pairs of values on each line delimited by a comma,\ne.g. plus, basic-page";
+
 		// add textfield to inputfields
 		$inputfields->add($f);
+
+
+    	$f = $this->modules->get('InputfieldTextarea');
+    	$f->attr('name', 'data_field');
+    	$f->label = 'Data';
+		$f->description = "Show field data in page tree list, add a list of fields to show along side its page name,\ne.g. counter \nname";
+
+		// add textfield to inputfields
+		$inputfields->add($f);
+
 
 		// return module config inputs
 	    return $inputfields;
--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 MarkInPageTree module for [Processwire]() 2.4+
 
-Enables you to show an icon next to a set of pages in the page tree that have a specified true file or template.
+Enables you to show an icon next to a set of pages in the page tree that have a specified true field or template. Also, it enables you to add a specific fields data in the page tree list along side it's title.
 
 ## Install
 [Install](http://modules.processwire.com/install-uninstall/) module in modules folder in your [Processwire](http://processwire.com/) installation. update modules in admin and install module.
 
 ## Settings
-- set field to check is true per page
-- set template name to check for
-- set icon to show in page tree when **either** of the above is true
+- 3 textareas for icon and field pairs, icon and template pairs, and list of fields
+-- icon to show next to page with field
+-- icon to show next to page of template name
+-- specific field data to be show after page title
 
 ## TODO:
-- enable users to set multiple fields, templates, and icons for each.
 
 ## License
 MIT
